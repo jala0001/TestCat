2326331970aa3a3c78ef416cbd85bb199b15e541
Jamie <jala0001@stud.kea.dk>
2023-11-08 11:02:22 +0100

    sfgsf

--- test/CatTest.java
+++ test/CatTest.java
@@ -1,4 +1,30 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CatTest {
-  
+    private Cat c;
+
+    @BeforeEach
+    void setup() {
+        c = new Cat("Jamie", 'M', 28.5);
+    }
+
+    @Test
+    void getName() {
+       assertNotNull(c.getName());
+       assertFalse(c.getName().isEmpty());
+    }
+
+    @Test
+    void getGender() {
+        assertTrue(c.getGender() == 'M' || c.getGender() == 'F');
+    }
+
+    @Test
+    void getWeightInKg() {
+        assertTrue(c.getWeightInKg() > 0 && c.getWeightInKg() <= 10);
+    }
 }
